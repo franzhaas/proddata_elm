5b0059ffe6fdc76ef2603e29e735d564b3a07312
franz <franz.dominik.haas@gmail.com>
2022-12-26 15:21:10 +0100

    - clenaed

--- src/Main.elm
+++ src/Main.elm
@@ -2,23 +2,11 @@ port module Main exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Events exposing (onClick)
 import Json.Decode exposing (Error(..), Value, decodeValue, string, int, decodeString, field, string, Decoder, map3)
 import Dict
-import Debug
 import List
 import Round
 import String
-
-
-
-type alias Lot_pf =
-    { lot : String
-    , pf_case : String
-    }
-
-compare : Lot_pf -> Lot_pf -> Order
-compare d1 d2 = if (d1.lot ++ d1.pf_case) > (d2.lot ++ d2.pf_case) then GT else if (d1.lot ++ d1.pf_case) == (d2.lot ++ d2.pf_case) then EQ else LT 
                     
 
 type alias ProdDataResults = Dict.Dict (String, String) Int
@@ -45,7 +33,6 @@ semiPreparedData prevPreparedData storage lot pf_case count =
         fails  = if pf_case == "fails" then Just count else Dict.get  (lot, "fails") storage
         passes = if pf_case == "passes" then Just count else Dict.get  (lot, "passes") storage
     in
-       
          case fails of
             Just jFails ->
                 case passes of
@@ -53,7 +40,6 @@ semiPreparedData prevPreparedData storage lot pf_case count =
                         Dict.insert  lot  (jPasses, jFails, (100.0*(toFloat jPasses)/(toFloat (jPasses+jFails)))) prevPreparedData
                     Nothing ->
                         Dict.insert  lot  (0, jFails, 0.0) prevPreparedData
-
             Nothing ->
                 case passes of
                     Just jPasses ->
@@ -70,21 +56,6 @@ prodDatDecoder =
     (field "count" int)
 
 -- VIEW
-type alias MyListItem =
-    { label : String
-    , price : Float
-    }
-
-total : Float
-total =
-    5.0
-
-myList : List MyListItem
-myList =
-    [ { label = "labelA", price = 2 }
-    , { label = "labelB", price = 3 }
-    ]
-
 toTableRow : ( String, ( Int, Int, Float ) ) -> Html Msg
 toTableRow myListItem =
     let
@@ -109,86 +80,32 @@ view model =
     in
         table
             []
-            ([ thead []
+            ( thead []
                 [ th [] [ text "LOT" ]
                 , th [] [ text "PASSES" ]
                 , th [] [ text "FAILS" ]
                 , th [] [ text "YIELD" ]
-                ]
-            ]
-                ++ List.map toTableRow preparedList
+                ] :: List.map toTableRow preparedList
             )
-viewDataFromJSOrError : Model -> Html Msg
-viewDataFromJSOrError model =
-    case model.jsonError of
-        Just error ->
-            viewError error
-
-        Nothing ->
-            let
-               oData = Debug.toString model.preparedData
-            in
-               viewDataFromJS oData
-
-
-viewError : Error -> Html Msg
-viewError jsonError =
-    let
-        errorHeading =
-            "Couldn't receive data from JavaScript"
-
-        errorMessage =
-            case jsonError of
-                Failure message _ ->
-                    message
-
-                _ ->
-                    "Error: Invalid JSON"
-    in
-    div []
-        [ h3 [] [ text errorHeading ]
-        , text ("Error: " ++ errorMessage)
-        ]
-
-
-viewDataFromJS : String -> Html msg
-viewDataFromJS data =
-    div []
-        [ br [] []
-        , strong [] [ text "Data received from JavaScript: " ]
-        , text data
-        ]
-
 
 type Msg
-    = SendDataToJS
-    | ReceivedDataFromJS Value
+    = ReceivedDataFromJS Value
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        SendDataToJS ->
-            ( model, Cmd.none )
-
         ReceivedDataFromJS value ->
             case decodeValue string value of
                 Ok data ->
-                    let 
-                        lpf = decodeString prodDatDecoder data
-                    in
-                        case lpf of
-                            Ok prodDat ->
-                                  let
-                                    lpf_key = (prodDat.lot, prodDat.pf_case)
-                                  in
-                                    let 
-                                       nextResult = Dict.insert lpf_key prodDat.count model.result
-                                    in
-                                        let
-                                                  nextPD = semiPreparedData model.preparedData nextResult prodDat.lot prodDat.pf_case prodDat.count
-                                        in
-                                              ( { model | result = nextResult, preparedData = nextPD }, Cmd.none )
-                            Err error ->
+                    case decodeString prodDatDecoder data of
+                        Ok prodDat ->
+                            let 
+                                pf_tuple = (prodDat.lot, prodDat.pf_case)
+                                nextResult = Dict.insert pf_tuple prodDat.count model.result
+                                nextPD = semiPreparedData model.preparedData nextResult prodDat.lot prodDat.pf_case prodDat.count
+                            in
+                                ( { model | result = nextResult, preparedData = nextPD }, Cmd.none )
+                        Err _ ->
                                   ( { model | dataFromJS = "Error parsing database content: " ++ data}, Cmd.none )
                 Err error ->
                     ( { model | jsonError = Just error }, Cmd.none )
