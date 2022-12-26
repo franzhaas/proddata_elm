f8a90d4aa055147df2bf502e05266c47680a81f9
franz <franz.dominik.haas@gmail.com>
2022-12-26 13:05:17 +0100

    - outputing data already parsed

--- src/Main.elm
+++ src/Main.elm
@@ -3,27 +3,42 @@ port module Main exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick)
-import Json.Decode exposing (Error(..), Value, decodeValue, string, int, decodeString, field, string, Decoder, map2)
+import Json.Decode exposing (Error(..), Value, decodeValue, string, int, decodeString, field, string, Decoder, map3)
 import Dict
+import Debug
 
+
+
+type alias Lot_pf =
+    { lot : String
+    , pf_case : String
+    }
+
+compare : Lot_pf -> Lot_pf -> Order
+compare d1 d2 = if (d1.lot ++ d1.pf_case) > (d2.lot ++ d2.pf_case) then GT else if (d1.lot ++ d1.pf_case) == (d2.lot ++ d2.pf_case) then EQ else LT 
+                    
+
+type alias ProdDataResults = Dict.Dict (String, String) Int
 
 type alias Model =
     { dataFromJS : String
     , jsonError : Maybe Error
+    , result : ProdDataResults
     }
 
-type alias Lot_pf =
-    { lot : String
-    , pf_case : Int
+type alias ProdDatType = 
+    {
+      lot : String
+    , pf_case : String
+    , count : Int
     }
 
-type alias Results = Dict.Dict Lot_pf Int
-
-lot_pfDecoder : Decoder Lot_pf
-lot_pfDecoder =
-  map2 Lot_pf
+prodDatDecoder : Decoder ProdDatType
+prodDatDecoder =
+  map3 ProdDatType
     (field "LOT" string)
-    (field "pf_case" int)
+    (field "type" string)
+    (field "count" int)
 
 
 view : Model -> Html Msg
@@ -41,7 +56,10 @@ viewDataFromJSOrError model =
             viewError error
 
         Nothing ->
-            viewDataFromJS model.dataFromJS
+            let
+               oData = Debug.toString model.result
+            in
+               viewDataFromJS oData
 
 
 viewError : Error -> Html Msg
@@ -87,13 +105,16 @@ update msg model =
             case decodeValue string value of
                 Ok data ->
                     let 
-                        lotDec = Json.Decode.decodeString (field "LOT" string) data
+                        lpf = decodeString prodDatDecoder data
                     in
-                        case lotDec of
-                            Ok lot ->
-                                  ( { model | dataFromJS = lot }, Cmd.none )
+                        case lpf of
+                            Ok prodDat ->
+                                  let
+                                    lpf_key = (prodDat.lot, prodDat.pf_case)
+                                  in
+                                    ( { model | result = Dict.insert lpf_key prodDat.count model.result }, Cmd.none )
                             Err error ->
-                                  (model , Cmd.none )
+                                  ( { model | dataFromJS = "Error parsing database content: " ++ data}, Cmd.none )
                 Err error ->
                     ( { model | jsonError = Just error }, Cmd.none )
 
@@ -111,6 +132,7 @@ initialModel : Model
 initialModel =
     { dataFromJS = ""
     , jsonError = Nothing
+    , result = Dict.empty 
     }
 
 main : Program () Model Msg
