d6e5418996604df0a5dfc40885dac47378695e23
franz <franz.dominik.haas@gmail.com>
2022-12-26 11:51:37 +0100

    parsing lot works

--- src/Main.elm
+++ src/Main.elm
@@ -3,52 +3,115 @@ port module Main exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick)
-import Dict exposing (Dict)
+import Json.Decode exposing (Error(..), Value, decodeValue, string, int, decodeString, field, string, Decoder, map2)
+import Dict
 
 
 type alias Model =
-    String
+    { dataFromJS : String
+    , jsonError : Maybe Error
+    }
+
+type alias Lot_pf =
+    { lot : String
+    , pf_case : Int
+    }
+
+type alias Results = Dict.Dict Lot_pf Int
+
+lot_pfDecoder : Decoder Lot_pf
+lot_pfDecoder =
+  map2 Lot_pf
+    (field "LOT" string)
+    (field "pf_case" int)
 
 
 view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick SendDataToJS ]
-            [ text "Send Data to JavaScript" ]
-        , br [] []
-        , br [] []
-        , text ("Data received from JavaScript: " ++ model )
+          [ text "Send Data to JavaScript" ]
+          , viewDataFromJSOrError model]
+
+
+viewDataFromJSOrError : Model -> Html Msg
+viewDataFromJSOrError model =
+    case model.jsonError of
+        Just error ->
+            viewError error
+
+        Nothing ->
+            viewDataFromJS model.dataFromJS
+
+
+viewError : Error -> Html Msg
+viewError jsonError =
+    let
+        errorHeading =
+            "Couldn't receive data from JavaScript"
+
+        errorMessage =
+            case jsonError of
+                Failure message _ ->
+                    message
+
+                _ ->
+                    "Error: Invalid JSON"
+    in
+    div []
+        [ h3 [] [ text errorHeading ]
+        , text ("Error: " ++ errorMessage)
+        ]
+
+
+viewDataFromJS : String -> Html msg
+viewDataFromJS data =
+    div []
+        [ br [] []
+        , strong [] [ text "Data received from JavaScript: " ]
+        , text data
         ]
 
 
 type Msg
     = SendDataToJS
-    | ReceivedDataFromJS Model
-
+    | ReceivedDataFromJS Value
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         SendDataToJS ->
-            ( model, sendData "Hello JavaScript!" )
+            ( model, Cmd.none )
 
-        ReceivedDataFromJS data ->
-            ( model ++ data, Cmd.none )
-
+        ReceivedDataFromJS value ->
+            case decodeValue string value of
+                Ok data ->
+                    let 
+                        lotDec = Json.Decode.decodeString (field "LOT" string) data
+                    in
+                        case lotDec of
+                            Ok lot ->
+                                  ( { model | dataFromJS = lot }, Cmd.none )
+                            Err error ->
+                                  (model , Cmd.none )
+                Err error ->
+                    ( { model | jsonError = Just error }, Cmd.none )
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    receiveData  ReceivedDataFromJS
+    receiveData ReceivedDataFromJS
 
-
-port sendData : String -> Cmd msg
-port receiveData : (Model -> msg) -> Sub msg
-
+port receiveData : (Value -> msg) -> Sub msg
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( "", Cmd.none )
+    ( initialModel, Cmd.none )
 
+initialModel : Model
+initialModel =
+    { dataFromJS = ""
+    , jsonError = Nothing
+    }
 
 main : Program () Model Msg
 main =
