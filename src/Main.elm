eca963bd1b5272e93f94389dfc2320a88434a0e1
franz <franz.dominik.haas@gmail.com>
2022-12-28 15:46:27 +0100

    - cleaned up, removed nesting of encoding of data

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,23 @@
 port module Main exposing (main)
-
 import Browser
-import Html exposing (..)
-import Json.Decode exposing (Error(..), Value, decodeValue, string, int, decodeString, field, string, Decoder, map3)
+import Html exposing (Html, text, td, tr, th, thead, table)
+import Json.Decode exposing (Error, Value, decodeValue, string, int, field, Decoder, map3, errorToString )
 import Dict
 import List
 import Round
 import String
                     
+type alias ProdDataResults  = Dict.Dict (String, String) Int
+type alias Model            = {jsError : Maybe Error, result : ProdDataResults, preparedData : PreperedDataType}
+type alias ProdDatType      = {lot : String, pf_case : String, count : Int}
+type alias PreperedDataType = Dict.Dict String (Int, Int, Float) 
 
-type alias ProdDataResults = Dict.Dict (String, String) Int
-
-type alias Model =
-    { jsError : Maybe Error
-    , jsonError : Maybe Error
-    , result : ProdDataResults
-    , preparedData : PreperedDataType
-    }
-
-type alias ProdDatType = 
-    {
-      lot : String
-    , pf_case : String
-    , count : Int
-    }
-
-type alias PreperedDataType =     Dict.Dict String (Int, Int, Float) 
+prodDatDecoder : Decoder ProdDatType
+prodDatDecoder =
+  map3 ProdDatType
+    (field "LOT" string)
+    (field "type" string)
+    (field "count" int)
 
 semiPreparedData : PreperedDataType -> ProdDataResults -> String -> String ->  Int -> PreperedDataType
 semiPreparedData prevPreparedData storage lot pf_case count =
@@ -46,16 +38,7 @@ semiPreparedData prevPreparedData storage lot pf_case count =
                         Dict.insert  lot  (jPasses, 0, 100.0) prevPreparedData
                     Nothing ->
                         prevPreparedData
-            
 
-prodDatDecoder : Decoder ProdDatType
-prodDatDecoder =
-  map3 ProdDatType
-    (field "LOT" string)
-    (field "type" string)
-    (field "count" int)
-
--- VIEW
 toTableRow : ( String, ( Int, Int, Float ) ) -> Html Msg
 toTableRow myListItem =
     let
@@ -64,27 +47,21 @@ toTableRow myListItem =
        fails = String.fromInt  fails1
        yield = Round.round 2  yield1
     in
-       tr []
-        [ td [] [ text lot ]
-        , td [] [ text passes ]
-        , td [] [ text fails ]
-        , td [] [ text yield ]
-        ]
+       tr [] [td [] [ text lot ], td [] [ text passes ] , td [] [ text fails ] , td [] [ text yield ]]
 
 view : Model -> Html Msg
 view model =
     let
         preparedList = Dict.toList    model.preparedData
     in
-        table
-            []
-            ( thead []
-                [ th [] [ text "LOT" ]
-                , th [] [ text "PASSES" ]
-                , th [] [ text "FAILS" ]
-                , th [] [ text "YIELD" ]
-                ] :: List.map toTableRow preparedList
-            )
+        case model.jsError of
+            Just error  ->
+                text ("JS error: " ++ (errorToString  error))
+            Nothing ->
+                table []
+                        (thead []
+                         [th [] [text "LOT" ], th [] [text "PASSES" ], th [] [text "FAILS" ], th [] [text "YIELD" ]]
+                         :: List.map toTableRow preparedList)
 
 type Msg
     = ReceivedDataFromJS Value
@@ -93,21 +70,17 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ReceivedDataFromJS value ->
-            case decodeValue string value of
-                Ok data ->
-                    case decodeString prodDatDecoder data of
-                        Ok prodDat ->
-                            let 
-                                pf_tuple = (prodDat.lot, prodDat.pf_case)
-                                nextResult = Dict.insert pf_tuple prodDat.count model.result
-                                nextPD = semiPreparedData model.preparedData nextResult prodDat.lot prodDat.pf_case prodDat.count
-                            in
-                                ({ model | result = nextResult, preparedData = nextPD }, Cmd.none)
-                        Err error ->
-                                  ({ model | jsError  = Just error}, Cmd.none)
+            case decodeValue prodDatDecoder value of
+                Ok prodDat ->
+                    let 
+                        pf_tuple = (prodDat.lot, prodDat.pf_case)
+                        nextResult = Dict.insert pf_tuple prodDat.count model.result
+                        nextPD = semiPreparedData model.preparedData nextResult prodDat.lot prodDat.pf_case prodDat.count
+                    in
+                        ({ model | result = nextResult, preparedData = nextPD }, Cmd.none)
                 Err error ->
-                    ({ model | jsonError = Just error }, Cmd.none )
-
+                        ({ model | jsError  = Just error}, Cmd.none)
+ 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     receiveData ReceivedDataFromJS
@@ -116,21 +89,8 @@ port receiveData : (Value -> msg) -> Sub msg
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( initialModel, Cmd.none )
-
-initialModel : Model
-initialModel =
-    { jsError = Nothing
-    , jsonError = Nothing
-    , result = Dict.empty 
-    , preparedData = Dict.empty
-    }
+    ({jsError = Nothing, result = Dict.empty, preparedData = Dict.empty}, Cmd.none )    
 
 main : Program () Model Msg
 main =
-    Browser.element
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
+    Browser.element {init = init, view = view, update = update, subscriptions = subscriptions}
